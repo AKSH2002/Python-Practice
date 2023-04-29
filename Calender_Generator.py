c18498189eca7a4ad87b419d60b5f6815dbee9af
Aksh Darji <akshdarji2019@gmail.com>
2023-04-29 22:10:09 +0530

    Initial started with 2 small programs

---
+++ Calender_Generator.py
@@ -0,0 +1,7 @@
+import calendar
+
+print("Current Year Calender")
+print(calendar.calendar(2023))
+
+print("Current Month Calender")
+print(calendar.month(2023,4))
---
+++ OTP_Generator.py
@@ -0,0 +1,4 @@
+import random
+
+print("The OTP is ")
+print(random.randint(1000,9999))
