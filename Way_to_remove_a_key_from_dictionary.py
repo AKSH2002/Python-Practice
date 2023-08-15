464bf9db57970effb5d36ac379cf6a4ee796328d
Aksh Darji <akshdarji2019@gmail.com>
2023-08-15 21:51:34 +0530

    Way to remove a key from dictionary program added

---
+++ Way_to_remove_a_key_from_dictionary.py
@@ -0,0 +1,8 @@
+test_dict = {"Aksh": 22, "Vinit": 21, "Jinang": 20}
+
+print("The dictionary before performing remove is : ", test_dict)
+
+del test_dict['Vinit']
+
+print("The dictionary after remove is : ", test_dict)
+
