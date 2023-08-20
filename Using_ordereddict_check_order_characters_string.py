8205d882dbb5ded2db81b68cdbef47fb7c98730b
Aksh Darji <akshdarji2019@gmail.com>
2023-08-20 21:02:49 +0530

    Using ordereddict check order characters string program added

---
+++ Using_ordereddict_check_order_characters_string.py
@@ -0,0 +1,20 @@
+from collections import OrderedDict
+
+def checkOrder(input, pattern):
+	
+	dict = OrderedDict.fromkeys(input)
+
+	ptrlen = 0
+	for key,value in dict.items():
+		if (key == pattern[ptrlen]):
+			ptrlen = ptrlen + 1
+		
+		if (ptrlen == (len(pattern))):
+			return 'true'
+
+	return 'false'
+
+if __name__ == "__main__":
+	input = 'Python Programming'
+	pattern = 'am'
+	print (checkOrder(input,pattern))
